ce235a4a783776ed5f3570157feca8b8a2aeb1f4
“Alienven” <alienven@qq.com>
2022-05-02 14:48:45 +0800

    updata configure

--- configure.sh
+++ configure.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
 # Xray generate configuration
 # Download and install Xray
-TMP_DIR="/tmp/xray"
+tmp_dir="/tmp/xray"
 config_path=$PROTOCOL"_ws_tls.json"
-DOWNLOAD_LINK="https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip"
-mkdir "$TMP_DIR"
-curl -L -H "Cache-Control: no-cache" -o "${TMP_DIR}/xray.zip" "$DOWNLOAD_LINK"
-unzip "${TMP_DIR}/xray.zip" -d "$TMP_DIR"
-install -m 755 "${TMP_DIR}/xray" /usr/local/bin/xray
-install -m 644 "${TMP_DIR}/geoip.dat" /usr/local/share/xray
-install -m 644 "${TMP_DIR}/geosite.dat" /usr/local/share/xray
+geodata_dir='/usr/local/share/xray'
+download_link_xray="https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip"
+download_link_geoip="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
+download_link_geosite="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
+mkdir "$tmp_dir"
+curl -L -H "Cache-Control: no-cache" -o "${tmp_dir}/xray.zip" "$download_link_xray"
+unzip "${tmp_dir}/xray.zip" -d "$tmp_dir"
+install -m 755 "${tmp_dir}/xray" /usr/local/bin/xray
+install -m 644 "${tmp_dir}/geoip.dat" /usr/local/share/xray
+install -m 644 "${tmp_dir}/geosite.dat" /usr/local/share/xray
+curl -H 'Cache-Control: no-cache' -o "${geodata_dir}/geoip.dat" "$download_link_geoip"
+curl -H 'Cache-Control: no-cache' -o "${geodata_dir}/geosite.dat" "$download_link_geosite"
 # Remove temporary directory
-rm -rf "$TMP_DIR"
+rm -rf "$tmp_dir"
 # xray new configuration
 install -d /usr/local/etc/xray
 envsubst '\$UUID,\$WS_PATH' < $config_path > /usr/local/etc/xray/config.json
