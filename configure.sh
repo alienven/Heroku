2ce7c49268acada7cdc02c60ef2390323ddbc707
“Alienven” <alienven@qq.com>
2022-05-02 15:15:02 +0800

    updata

--- configure.sh
+++ configure.sh
@@ -5,14 +5,14 @@ tmp_dir="/tmp/xray"
 config_path=$PROTOCOL"_ws_tls.json"
 geodata_dir='/usr/local/share/xray'
 download_link_xray="https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip"
-mkdir "$tmp_dir"
-curl -L -H "Cache-Control: no-cache" -o "${tmp_dir}/xray.zip" "$download_link_xray"
-unzip "${tmp_dir}/xray.zip" -d "$tmp_dir"
+mkdir "${tmp_dir}"
+curl -L -H "Cache-Control: no-cache" -o "${tmp_dir}/xray.zip" "${download_link_xray}"
+unzip "${tmp_dir}/xray.zip" -d "${tmp_dir}"
 install -m 755 "${tmp_dir}/xray" /usr/local/bin/xray
-install -m 644 "${tmp_dir}/geoip.dat" "$geodata_dir"
-install -m 644 "${tmp_dir}/geosite.dat" "$geodata_dir"
+install -m 644 "${tmp_dir}/geoip.dat" "${geodata_dir}"/geoip.dat
+install -m 644 "${tmp_dir}/geosite.dat" "${geodata_dir}"/geosite.dat
 # Remove temporary directory
-rm -rf "$tmp_dir"
+rm -rf "${tmp_dir}"
 # xray new configuration
 install -d /usr/local/etc/xray
 envsubst '\$UUID,\$WS_PATH' < $config_path > /usr/local/etc/xray/config.json
